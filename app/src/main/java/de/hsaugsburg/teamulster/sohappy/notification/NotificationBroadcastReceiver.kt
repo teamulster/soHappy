ef5a31d03c6bb52149a5f0f52e0770655b224c77
limkokyew <lim.kokyew16@gmail.com>
2020-09-03 15:23:34 +0200

    Added comments and fixed Detekt error

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/notification/NotificationBroadcastReceiver.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/notification/NotificationBroadcastReceiver.kt
@@ -7,7 +7,19 @@ import android.content.Intent
 import androidx.core.app.NotificationManagerCompat
 import de.hsaugsburg.teamulster.sohappy.R
 
+/**
+ * Receives notification alarms from NotificationHandler and uses a NotificationManager
+ * to push the notification to the user's device.
+ */
 class NotificationBroadcastReceiver: BroadcastReceiver() {
+
+    /**
+     * Upon receiving an alarm from NotificationHandler, the corresponding notification is
+     * retrieved using its intent and pushed to the user's device.
+     *
+     * @param context Activity that sent the notification
+     * @param intent Intent that was attached to the alarm
+     */
     override fun onReceive(context: Context, intent: Intent) {
         val notification = intent.getParcelableExtra<Notification>(
             context.getString(R.string.notification_name)
--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/notification/NotificationHandler.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/notification/NotificationHandler.kt
@@ -1,6 +1,10 @@
 package de.hsaugsburg.teamulster.sohappy.notification
 
-import android.app.*
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -10,9 +14,22 @@ import androidx.core.app.NotificationManagerCompat
 import de.hsaugsburg.teamulster.sohappy.CameraActivity
 import de.hsaugsburg.teamulster.sohappy.R
 
+/**
+ * NotificationHandler handles all actions concerning notifications, such as creating
+ * Notification Channels or scheduling notifications to remind the user of something.
+ */
 class NotificationHandler private constructor() {
     companion object {
+
+        /**
+         * Schedules a notification telling the user to open the app after the provided
+         * delay.
+         *
+         * @param context Activity that is trying to schedule a notification.
+         * @param delay Delay in milliseconds specifying when the alarm should be triggered.
+         */
         fun scheduleNotification(context: Context, delay: Long) {
+            // Create a PendingIntent that starts the app when triggered
             val activityIntent = Intent(context, CameraActivity::class.java)
             val activityPendingIntent: PendingIntent? = TaskStackBuilder.create(context).run {
                 addNextIntentWithParentStack(activityIntent)
@@ -30,6 +47,8 @@ class NotificationHandler private constructor() {
                 setPriority(NotificationCompat.PRIORITY_DEFAULT)
             }.build()
 
+            // Schedule the notification using a PendingIntent that is passed to the system's
+            // AlarmManager and triggered after a certain delay
             val scheduleTime = SystemClock.elapsedRealtime() + delay
             val scheduleIntent = Intent(context, NotificationBroadcastReceiver::class.java)
             scheduleIntent.putExtra(context.getString(R.string.notification_name), notification)
@@ -49,9 +68,15 @@ class NotificationHandler private constructor() {
             )
         }
 
+        /**
+         * Starting from Android 8.0, all notifications must be associated with a Notification
+         * Channel. This function's purpose is to create a Notification Channel for this app
+         * if it does not already exist.
+         *
+         * @param context Activity that requests the creation of a new Notification Channel.
+         */
         fun createNotificationChannel(context: Context) {
-            // To ensure compatibility with newer Android versions, a notification channel has
-            // to be created for notifications to be pushed into
+            // Run the code only if Android version is at least 8.0
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 val name = context.resources.getString(R.string.channel_name)
                 val descriptionText = context.resources.getString(R.string.channel_description)
