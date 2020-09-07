229d69db15e4330f04f0f2a22a9388bd22f8f0bb
Fabian Siegel <fabians1999@gmail.com>
2020-09-07 16:16:48 +0200

    Preview thingy

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/analyzer/ImageAnalyzer.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/analyzer/ImageAnalyzer.kt
@@ -3,10 +3,13 @@ package de.hsaugsburg.teamulster.sohappy.analyzer
 import android.app.Activity
 import android.graphics.Bitmap
 import android.util.Log
+import android.widget.TextView
 import de.hsaugsburg.teamulster.sohappy.CameraActivity
+import de.hsaugsburg.teamulster.sohappy.R
 import de.hsaugsburg.teamulster.sohappy.analyzer.detector.FaceDetector
 import de.hsaugsburg.teamulster.sohappy.analyzer.detector.SmileDetector
 import de.hsaugsburg.teamulster.sohappy.config.ImageAnalyzerConfig
+import kotlinx.android.synthetic.main.activity_camera.view.*
 import kotlin.concurrent.thread
 
 class ImageAnalyzer (val activity: CameraActivity, config: ImageAnalyzerConfig) {
@@ -39,6 +42,12 @@ class ImageAnalyzer (val activity: CameraActivity, config: ImageAnalyzerConfig) 
                 val bitmap = this.activity.queue.poll()
                 if (bitmap != null) {
                     val result = computeSmileDetectionResult(bitmap)
+                    val tv = activity.findViewById<TextView>(R.id.textView)
+                    if (result != null) {
+                        tv.text = if (result.isSmiling) "isSmiling"  else "noSmile"
+                    } else {
+                        tv.text = "No Face"
+                    }
                     Log.d("Result:", result.toString())
                 }
             }
