969038b3ffc52e3d6e331c32bc507ec6359cef7f
limkokyew <lim.kokyew16@gmail.com>
2020-09-09 16:19:08 +0200

    Added first draft of thescreens shown in the paper

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/HomeFragment.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/HomeFragment.kt
@@ -2,9 +2,12 @@ package de.hsaugsburg.teamulster.sohappy
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import de.hsaugsburg.teamulster.sohappy.databinding.FragmentHomeBinding
 
@@ -35,6 +38,11 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        (requireActivity() as AppCompatActivity).supportActionBar!!.show()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.overflow_menu, menu)
@@ -57,6 +65,10 @@ class HomeFragment : Fragment() {
         )
         circleAnimation.duration = 300
         view.visibility = View.VISIBLE
+
+        circleAnimation.doOnEnd {
+            findNavController().navigate(R.id.smileFragment)
+        }
 
         circleAnimation.start()
     }
---
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/SmileFragment.kt
@@ -0,0 +1,152 @@
+package de.hsaugsburg.teamulster.sohappy
+
+import android.graphics.drawable.Animatable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import de.hsaugsburg.teamulster.sohappy.databinding.FragmentSmileBinding
+
+// TODO: The requireView.postDelayed() calls serve as proof of concept for animations - replace!
+class SmileFragment : Fragment() {
+    private lateinit var binding: FragmentSmileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_smile,
+            container,
+            false
+        )
+
+        (requireActivity() as AppCompatActivity).supportActionBar!!.hide()
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // The initial animation has to be started here, otherwise the animation
+        // will play even if the screen is currently not focused on this fragment
+        startInitAnimation()
+
+        requireView().postDelayed({
+            (binding.checkmarkView.drawable as Animatable).start()
+        }, 3000)
+
+        requireView().postDelayed({
+            startCountdown()
+        }, 4000)
+
+        requireView().postDelayed({
+            fadeOutText()
+        }, 7750)
+
+        requireView().postDelayed({
+            fadeInText(getString(R.string.fragment_camera_stimulus1))
+        }, 8250)
+    }
+
+    private fun startInitAnimation() {
+        binding.fadeInView.animate()
+            .alpha(0f)
+            .setDuration(1500)
+            .withEndAction {
+                binding.fadeInView.visibility = View.INVISIBLE
+                fadeInText(getString(R.string.fragment_camera_init))
+            }
+    }
+
+    private fun startCountdown() {
+        binding.checkmarkView.animate()
+            .alpha(0f)
+            .setDuration(500)
+
+        fadeOutText()
+        requireView().postDelayed({
+            fadeInText(getString(R.string.fragment_camera_init))
+
+            binding.countdownText.apply {
+                alpha = 0f
+                visibility = View.VISIBLE
+            }
+            binding.countdownText.animate()
+                .alpha(1f)
+                .setDuration(500)
+            (binding.countdownView.drawable as Animatable).start()
+        }, 750)
+
+        requireView().postDelayed({
+            tickCountdown()
+        }, 1500)
+
+        requireView().postDelayed({
+            tickCountdown()
+        }, 2500)
+
+        requireView().postDelayed({
+            tickCountdown()
+        }, 3500)
+
+        /* requireView().postDelayed({
+            binding.countdownText.animate()
+                .alpha(0f)
+                .translationYBy(100f)
+                .setDuration(125)
+        }, 1250)
+
+        requireView().postDelayed({
+            binding.countdownText.setText("2")
+            binding.countdownText.animate()
+                .alpha(1f)
+                .setDuration(125)
+        }, 1375) */
+    }
+
+    private fun tickCountdown() {
+        val nextTick = binding.countdownText.text.toString().toInt() - 1
+        binding.countdownText.animate()
+            .alpha(0f)
+            .translationYBy(100f)
+            .setDuration(125)
+            .withEndAction {
+                binding.countdownText.apply {
+                    y -= 200f
+                    text = nextTick.toString()
+                }
+
+                if (nextTick > 0) {
+                    binding.countdownText.animate()
+                        .alpha(1f)
+                        .translationYBy(100f)
+                        .setDuration(125)
+                }
+            }
+    }
+
+    private fun fadeInText(text: String) {
+        binding.textView.apply {
+            setText(text)
+            alpha = 0f
+            visibility = View.VISIBLE
+        }
+
+        binding.textView.animate()
+            .alpha(1f)
+            .setDuration(500)
+    }
+
+    private fun fadeOutText() {
+        binding.textView.animate()
+            .alpha(0f)
+            .setDuration(500)
+    }
+}
