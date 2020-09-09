ae9f5196a5a72a4b04f7ee9e94691583761d68c4
diehm <Lukas.Diehm@hs-Augsburg.de>
2020-09-09 10:34:52 +0200

    refactor Assert

--- app/src/androidTest/java/de/hsaugsburg/teamulster/sohappy/HaarCascadeFaceDetectorTest.kt
+++ app/src/androidTest/java/de/hsaugsburg/teamulster/sohappy/HaarCascadeFaceDetectorTest.kt
@@ -7,10 +7,10 @@ import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import de.hsaugsburg.teamulster.sohappy.analyzer.detector.facedetectorimpl.HaarCascadeFaceDetector
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import junit.framework.Assert.assertTrue as assertTrue
 
 @RunWith(AndroidJUnit4::class)
 class HaarCascadeFaceDetectorTest {
@@ -31,13 +31,13 @@ class HaarCascadeFaceDetectorTest {
             val istream = instrumentationContext.assets.open("test.jpg")
             val bitmap = BitmapFactory.decodeStream(istream);
             val detectResult = haarCascadeFaceDetector.detect(bitmap)
-            assert(detectResult != null)
+            assertTrue(detectResult != null)
         }
         scenario.onActivity {
             val istream = instrumentationContext.assets.open("negative-test.jpg")
             val bitmap = BitmapFactory.decodeStream(istream);
             val detectResult = haarCascadeFaceDetector.detect(bitmap)
-            assert(detectResult == null)
+            assertTrue(detectResult == null)
         }
     }
 }
