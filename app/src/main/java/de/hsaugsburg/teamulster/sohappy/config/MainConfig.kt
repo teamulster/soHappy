cc931a84b8a3132df434bd202be6efd2959638e5
Marcel Arndt <master.tww@web.de>
2020-09-10 15:02:01 +0200

    Include AboutCOnfig in COnfigManager and write tests

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/config/MainConfig.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/config/MainConfig.kt
@@ -3,6 +3,7 @@ package de.hsaugsburg.teamulster.sohappy.config
 /**
  * This data class stores the MainConfig.
  *
- * @property imageAnalyzerConfig
+ * @property [imageAnalyzerConfig]
+ * @property [aboutConfig]
  */
-data class MainConfig(val imageAnalyzerConfig: ImageAnalyzerConfig)
+data class MainConfig(val imageAnalyzerConfig: ImageAnalyzerConfig, val aboutConfig: AboutConfig)
