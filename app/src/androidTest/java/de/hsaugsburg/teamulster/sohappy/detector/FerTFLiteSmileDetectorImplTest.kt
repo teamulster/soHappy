67af0cf7a55d4edda8ffc003d4207f7f4e36e9ab
Marcel Arndt <master.tww@web.de>
2020-09-10 09:28:46 +0200

    Replace assertEquals with assertTrue/assertFalse if possible

--- app/src/androidTest/java/de/hsaugsburg/teamulster/sohappy/detector/FerTFLiteSmileDetectorImplTest.kt
+++ app/src/androidTest/java/de/hsaugsburg/teamulster/sohappy/detector/FerTFLiteSmileDetectorImplTest.kt
@@ -8,7 +8,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import de.hsaugsburg.teamulster.sohappy.CameraActivity
 import de.hsaugsburg.teamulster.sohappy.analyzer.detector.FerTFLiteSmileDetectorImpl
-import junit.framework.Assert.*
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
