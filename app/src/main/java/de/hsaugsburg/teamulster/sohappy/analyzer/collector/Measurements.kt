2cd1fd52bb53ba266d5b40a175b12ffc072dc7b1
Marcel Arndt <master.tww@web.de>
2020-09-10 15:53:15 +0200

    Make detekt compliant

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/analyzer/collector/Measurements.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/analyzer/collector/Measurements.kt
@@ -2,9 +2,17 @@ package de.hsaugsburg.teamulster.sohappy.analyzer.collector
 
 import de.hsaugsburg.teamulster.sohappy.analyzer.detector.DetectionResult
 
+/**
+ * This class hold a list of DetectionResults.
+ * */
 class Measurements {
     private val results : ArrayList<DetectionResult> = ArrayList()
 
+    /**
+     * This function adds a DetectionResult to the results list.
+     *
+     * @param [detectionResult] a DetectionResult object which will be stored
+     * */
     fun add(detectionResult: DetectionResult) {
         results.add(detectionResult)
     }
--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/analyzer/detector/DetectionResult.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/analyzer/detector/DetectionResult.kt
@@ -1,5 +1,11 @@
 package de.hsaugsburg.teamulster.sohappy.analyzer.detector
 
+/**
+ * This data class stores faceDetectionResult / smileDetectionResult of a analyzed frame.
+ *
+ * @property [faceDetectionResult]
+ * @property [smileDetectionResult]
+ * */
 data class DetectionResult(
     val faceDetectionResult: FaceDetector.Companion.FaceDetectionResult?,
     val smileDetectionResult: SmileDetector.Companion.SmileDetectionResult?
