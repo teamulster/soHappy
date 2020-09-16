8e125e6abef61a28cf09d7d77d60222b4cea62b3
Fabian Siegel <fabians1999@gmail.com>
2020-09-16 13:23:12 +0200

    stateM

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/CameraActivity.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/CameraActivity.kt
@@ -18,7 +18,7 @@ import java.io.IOException
  */
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
-    internal val stateMachine: StateMachine by viewModels()
+    private val stateMachine: StateMachine by viewModels()
     internal lateinit var localDatabaseManager : LocalDatabaseManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
