3e87e8154510d48e5096115359ee2ed39094bf25
diehm <Lukas.Diehm@hs-Augsburg.de>
2020-09-16 15:18:07 +0200

    renamed CameraActivity to MainActivity

--- app/src/androidTest/java/de/hsaugsburg/teamulster/sohappy/analyzer/detector/GoogleMLKitAPIFaceDetectorTest.kt
+++ app/src/androidTest/java/de/hsaugsburg/teamulster/sohappy/analyzer/detector/GoogleMLKitAPIFaceDetectorTest.kt
@@ -5,7 +5,7 @@ import android.graphics.BitmapFactory
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.launchActivity
 import androidx.test.platform.app.InstrumentationRegistry
-import de.hsaugsburg.teamulster.sohappy.CameraActivity
+import de.hsaugsburg.teamulster.sohappy.MainActivity
 import de.hsaugsburg.teamulster.sohappy.analyzer.detector.facedetectorimpl.GoogleMLKitAPIFaceDetector
 import junit.framework.Assert.assertTrue
 import org.junit.Before
@@ -13,13 +13,13 @@ import org.junit.Test
 
 class GoogleMLKitAPIFaceDetectorTest {
     private lateinit var mLKitFaceDetector: GoogleMLKitAPIFaceDetector
-    private lateinit var scenario: ActivityScenario<CameraActivity>
+    private lateinit var scenario: ActivityScenario<MainActivity>
     private val instrumentationContext: Context =
         InstrumentationRegistry.getInstrumentation().context
 
     @Before
     fun setUp() {
-        scenario = launchActivity<CameraActivity>()
+        scenario = launchActivity<MainActivity>()
         scenario.onActivity { mLKitFaceDetector = GoogleMLKitAPIFaceDetector(it) }
     }
 
