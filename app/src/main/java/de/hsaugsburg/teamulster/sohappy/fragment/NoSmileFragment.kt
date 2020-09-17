49b7f3ccdc6ec8bca91d4be98db09b499cc75009
diehm <Lukas.Diehm@hs-Augsburg.de>
2020-09-17 13:19:39 +0200

    added mat.release()

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/fragment/NoSmileFragment.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/fragment/NoSmileFragment.kt
@@ -1,5 +1,7 @@
 package de.hsaugsburg.teamulster.sohappy.fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
