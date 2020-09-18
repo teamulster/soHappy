c4d585e2ab4ff55c97caca6723459d129423d3f9
diehm <Lukas.Diehm@hs-Augsburg.de>
2020-09-18 13:14:51 +0200

    fixed crash on the second run

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/VideoMasker.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/VideoMasker.kt
@@ -9,77 +9,71 @@ import jp.co.cyberagent.android.gpuimage.filter.*
 object VideoMasker {
 
     lateinit var gpuImageView: GPUImageView
-
-    private val blueFilter: GPUImageFilter = GPUImageFilterGroup(
-        listOf(
-            GPUImagePixelationFilter().apply { setPixel(5F) },
-            GPUImageGaussianBlurFilter(5.0f),
-            GPUImageContrastFilter(1f),
-            GPUImageSepiaToneFilter(),
-            GPUImageHueFilter(210f),
-            GPUImageSaturationFilter(1f),
-            GPUImageBrightnessFilter(-0.2f)
-        )
-    )
-    private val redFilter: GPUImageFilter = GPUImageFilterGroup(
-        listOf(
-            GPUImagePixelationFilter().apply { setPixel(5F) },
-            GPUImageGaussianBlurFilter(5.0f),
-            GPUImageContrastFilter(1f),
-            GPUImageSepiaToneFilter(),
-            GPUImageHueFilter(330f),
-            GPUImageSaturationFilter(1f),
-            GPUImageBrightnessFilter(-0.2f)
-        )
-    )
-    private val yellowFilter: GPUImageFilter = GPUImageFilterGroup(
-        listOf(
-            GPUImagePixelationFilter().apply { setPixel(5F) },
-            GPUImageGaussianBlurFilter(5.0f),
-            GPUImageContrastFilter(1f),
-            GPUImageSepiaToneFilter(),
-            GPUImageHueFilter(30f),
-            GPUImageSaturationFilter(1f),
-            GPUImageBrightnessFilter(-0.2f)
-        )
-    )
-    private val greenFilter: GPUImageFilter = GPUImageFilterGroup(
-        listOf(
-            GPUImagePixelationFilter().apply { setPixel(5F) },
-            GPUImageGaussianBlurFilter(5.0f),
-            GPUImageContrastFilter(1f),
-            GPUImageSepiaToneFilter(),
-            GPUImageHueFilter(90f),
-            GPUImageSaturationFilter(1f),
-            GPUImageBrightnessFilter(-0.2f)
-        )
-    )
-
     /**
      * applies blue blur filter.
      */
     fun applyBlueFilter() {
-        gpuImageView.filter = blueFilter
+        gpuImageView.filter = GPUImageFilterGroup(
+            listOf(
+                GPUImagePixelationFilter().apply { setPixel(5F) },
+                GPUImageGaussianBlurFilter(5.0f),
+                GPUImageContrastFilter(1f),
+                GPUImageSepiaToneFilter(),
+                GPUImageHueFilter(210f),
+                GPUImageSaturationFilter(1f),
+                GPUImageBrightnessFilter(-0.2f)
+            )
+        )
     }
 
     /**
      * applies red blur filter.
      */
     fun applyRedFilter() {
-        gpuImageView.filter = redFilter
+        gpuImageView.filter = GPUImageFilterGroup(
+            listOf(
+                GPUImagePixelationFilter().apply { setPixel(5F) },
+                GPUImageGaussianBlurFilter(5.0f),
+                GPUImageContrastFilter(1f),
+                GPUImageSepiaToneFilter(),
+                GPUImageHueFilter(330f),
+                GPUImageSaturationFilter(1f),
+                GPUImageBrightnessFilter(-0.2f)
+            )
+        )
     }
 
     /**
      * applies yellow blur filter.
      */
     fun applyYellowFilter() {
-        gpuImageView.filter = yellowFilter
+        gpuImageView.filter = GPUImageFilterGroup(
+            listOf(
+                GPUImagePixelationFilter().apply { setPixel(5F) },
+                GPUImageGaussianBlurFilter(5.0f),
+                GPUImageContrastFilter(1f),
+                GPUImageSepiaToneFilter(),
+                GPUImageHueFilter(30f),
+                GPUImageSaturationFilter(1f),
+                GPUImageBrightnessFilter(-0.2f)
+            )
+        )
     }
 
     /**
      * applies green blur filter.
      */
     fun applyGreenFilter() {
-        gpuImageView.filter = greenFilter
+        gpuImageView.filter = GPUImageFilterGroup(
+            listOf(
+                GPUImagePixelationFilter().apply { setPixel(5F) },
+                GPUImageGaussianBlurFilter(5.0f),
+                GPUImageContrastFilter(1f),
+                GPUImageSepiaToneFilter(),
+                GPUImageHueFilter(90f),
+                GPUImageSaturationFilter(1f),
+                GPUImageBrightnessFilter(-0.2f)
+            )
+        )
     }
 }
