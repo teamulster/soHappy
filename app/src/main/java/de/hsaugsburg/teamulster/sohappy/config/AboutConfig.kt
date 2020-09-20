bea4e4f38014b5772c1072587bc60bca4ad3c697
diehm <Lukas.Diehm@hs-Augsburg.de>
2020-09-20 16:17:23 +0200

    added url to Help -> feedback, issue, licens

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/config/AboutConfig.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/config/AboutConfig.kt
@@ -6,5 +6,9 @@ package de.hsaugsburg.teamulster.sohappy.config
  * @property [creditsURL] a URL to credits reference
  * @property [privacyURL] a URL to privacy reference
  * @property [imprintURL] a URL to imprint reference
+ * @property [licenseURL] a URL to license reference
+ * @property [feedbackURL] a URL to send feedback
+ * @property [issueURL] a URL to report Issue
  */
-data class AboutConfig(val creditsURL: String, val privacyURL: String, val imprintURL: String)
+data class AboutConfig(val creditsURL: String, val privacyURL: String, val imprintURL: String,
+                       val licenseURL: String, val feedbackURL: String, val issueURL: String)
