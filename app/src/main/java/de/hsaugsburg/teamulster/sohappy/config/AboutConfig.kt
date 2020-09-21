bb727cea37c2a689f10152b0e02bc2a05153bbea
Marcel Arndt <master.tww@web.de>
2020-09-21 11:02:10 +0200

    Add Builder to AboutConfig

--- app/src/main/java/de/hsaugsburg/teamulster/sohappy/config/AboutConfig.kt
+++ app/src/main/java/de/hsaugsburg/teamulster/sohappy/config/AboutConfig.kt
@@ -7,4 +7,15 @@ package de.hsaugsburg.teamulster.sohappy.config
  * @property [privacyURL] a URL to privacy reference
  * @property [imprintURL] a URL to imprint reference
  */
-data class AboutConfig(val creditsURL: String, val privacyURL: String, val imprintURL: String)
+data class AboutConfig(val creditsURL: String, val privacyURL: String, val imprintURL: String) {
+    class Builder(
+        private var creditsURL: String = "",
+        private var privacyURL: String = "",
+        private var imprintURL: String = ""
+    ) {
+        fun setCreditsURL(creditsURL: String) = apply { this.creditsURL = creditsURL }
+        fun setPrivacyURL(privacyURL: String) = apply { this.privacyURL = privacyURL }
+        fun setImprintURL(imprintURL: String) = apply { this.imprintURL = imprintURL }
+        fun build(): AboutConfig = AboutConfig(creditsURL, privacyURL, imprintURL)
+    }
+}
